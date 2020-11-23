81de831a2bc0118367f375bcc184fec88c9004ea
SkootNoog <scottlnegus@gmail.com>
2020-11-22 18:26:21 -0600

    Fixed search by adding eager loading to tags

--- src/datamodel/Listing.java
+++ src/datamodel/Listing.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -34,7 +35,7 @@ public class Listing {
    @JoinColumn(name="fk_category")	// Foreign Key
    private Category category;
    
-   @ManyToMany
+   @ManyToMany(fetch = FetchType.EAGER)
    @JoinTable(
 		   name = "listing_tag", 
 		   joinColumns = { 
